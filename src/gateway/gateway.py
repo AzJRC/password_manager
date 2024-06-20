6f983364bb3a171550df4528027c302d485c901b
AzJRC <rodajrc@gmail.com>
2024-06-20 07:07:55 +0000

    Gateway: started to code the entrypoint to auth_service

--- src/gateway/gateway.py
+++ src/gateway/gateway.py
@@ -1,12 +1,26 @@
 import logging, json, datetime, os, requests
 from fastapi.testclient import TestClient
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi import FastAPI, Request, Depends, HTTPException
+from dotenv import load_dotenv
 
 # Logging for debugging
 LOGGING = True
 if LOGGING:
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
+
+# Environment Variables
+load_dotenv()
+
+# Microservices
+MS_URLS = {
+    'AUTH_SERVICE_URL': os.getenv('AUTH_SERVICE_URL', 'http://localhost:8001'),
+    'VAULT_SERVICE_URL': os.getenv('VAULT_SERVICE_URL', 'http://localhost:8002')
+}
+
+# Parameters
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def run_gateway():
@@ -22,12 +36,28 @@ def run_gateway():
     # Authentiction service routes
     
     @server.post("/register")
-    async def auth_register():
-        pass
+    async def auth_register(request: Request):
+        data = await request.json()
+        if LOGGING:
+            logger.info("Register request received: %s", data)
+       
+        # Verify request
+
+        # Forward request
+        response = requests.post(f"{MS_URLS['AUTH_SERVICE_URL']}/register", json=data)
+        if response.status_code != 200:
+            if LOGGING:
+                logger.error("- Registration failed (10): %s", response.text)
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+
+        if LOGGING:
+            logger.info("Registration succesfull (11): %s", response.json())
+
+    # form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 
     @server.post("/login")
     async def auth_login():
-        pass
+        pass      
 
     # Vault service routes
     
@@ -52,6 +82,9 @@ def run_gateway():
         pass
 
     return server
+
+
+server = run_gateway()
 
 
 # Unit tests
